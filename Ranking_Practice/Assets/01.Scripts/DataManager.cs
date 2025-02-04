0a09600a6e6aaf946080e78cd4cdc6ada11707c1
Sean-mn <b01080080329@gmail.com>
2025-02-04 17:20:38 +0900

    Update DataManager

---
+++ Ranking_Practice/Assets/01.Scripts/DataManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class DataManager : MonoSingleton<DataManager>
+{
+    public int score;
+
+    // 상위 5위 저장
+    public string[] bestNames = new string[5];
+    public int[] bestScores = new int[5];
+
+    public void SetScore(int curScore, string curName)
+    {
+        PlayerPrefs.SetString("CurrentPlayerName", curName);
+        PlayerPrefs.SetInt("CurrentPlayerScore", curScore);
+
+        for (int i = 0; i < 5; i++)
+        {
+            bestScores[i] = PlayerPrefs.GetInt(i + "BestScore", 0);
+            bestNames[i] = PlayerPrefs.GetString(i + "BestName", " ");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (curScore > bestScores[i])
+            {
+                for (int j = 4; j > i; j--)
+                {
+                    bestScores[j] = bestScores[j - 1];
+                    bestNames[j] = bestNames[j - 1];
+                }
+
+                bestScores[i] = curScore;
+                bestNames[i] = curName;
+                break;
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            PlayerPrefs.SetInt(i + "BestScore", bestScores[i]);
+            PlayerPrefs.SetString(i + "BestName", bestNames[i]);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PlayerPrefs.DeleteAll();
+
+            for (int i = 0; i < 5; i++)
+            {
+                bestScores[i] = PlayerPrefs.GetInt(i + "BestScore", 0);
+                bestNames[i] = PlayerPrefs.GetString(i + "BestName", " ");
+            }
+        }
+    }
+}
